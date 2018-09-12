b5bfd44acda6677b45f180b561b117f9188d22a3
bdalce <bdalce@terpmail.umd.edu>
2018-09-12 12:16:51 -0400

    Add files via upload

---
+++ Extracting DOI dates.py
@@ -0,0 +1,71 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from crossref.restful import Works
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+FileLoc = "C:\\Users\\briana.dalce\\Documents\\all_publications_sample_new.xlsx"
+
+
+# In[4]:
+
+
+data = pd.read_excel(FileLoc, usecols = 3)
+
+
+# In[5]:
+
+
+data = data.dropna()
+
+
+# In[6]:
+
+
+pub = Works()
+  
+   
+
+
+# In[7]:
+
+
+for i,j in data.iterrows():
+    x = pub.doi(j['DOI']) #finds DOI info for each cell of the DOI column
+    y = j['Record Number']
+    if x is None: #handles cells that do not have DOIs or incorrect DOIs
+        print('')
+    else:
+        print(y, x['created'], end = '\n') #prints only the timestamp for cells stored in x 
+
+
+# In[8]:
+
+
+#def pubDate(list):
+#    for i,j in data.iterrows():
+#        x = pub.doi(j['DOI']) #finds DOI info for each cell
+#        y = j['Record Number']
+#        if x is None: #handles cells that do not have DOIs or incorrect DOIs
+#            print('')
+#        else:
+#            print(y, x['created'], end = '\n') #prints only the timestamp for cells stored in x 
+
+
+# In[9]:
+
+
+#pubDate(FileLoc)
+
