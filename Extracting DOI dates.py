661a9ea70b0c3658a2cb1e9a7cc394ac79e4554a
Briana Dalce <bdalce@terpmail.umd.edu>
2018-10-12 13:50:07 -0400

    Update Extracting DOI dates.py

--- Extracting DOI dates.py
+++ Extracting DOI dates.py
@@ -15,15 +15,21 @@ import pandas as pd
 
 # In[3]:
 
-
-FileLoc = "C:\\Users\\briana.dalce\\Documents\\all_publications_sample_new.xlsx"
+File = input("xlsx or csv: ")
+if File == 'xlsx':
+    FileLoc = input("Enter complete file path: ")
+    data = pd.read_excel(FileLoc)
+elif File == 'csv':
+    FileLoc = input("Enter complete file path: ")
+    data = pd.read_csv(FileLoc)
+else:
+    print("Not valid")
 
 
 # In[4]:
 
-
-data = pd.read_excel(FileLoc, usecols = 3) #using only columns 0-3
-
+data = data.dropna(subset=['DOI'])
+pub = Works()
 
 # In[5]:
 
@@ -34,20 +40,14 @@ data = data.dropna() #drops the null values within the first four columns of the
 # In[6]:
 
 
-pub = Works()
-  
-   
-
-
-# In[7]:
-
-
 for i,j in data.iterrows():
     x = pub.doi(j['DOI']) #finds DOI info for each cell of the DOI column
-    y = j['Record Number']
     if x is None: #handles cells that do not have DOIs or incorrect DOIs
         print('')
     else:
-        print(y, x['created'], end = '\n') #prints only the timestamp for cells stored in x 
+        print(i, x['created'], end = '\n') #prints only the timestamp for cells stored in x 
+  
+   
+
 
 
