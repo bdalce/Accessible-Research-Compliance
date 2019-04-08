d4f757a0e7ba911819966e2448e441f5f6e2f269
Briana Dalce <bdalce@terpmail.umd.edu>
2019-04-08 12:42:20 -0400

    Update Public Access Journals Script .py

--- Public Access Journals Script .py
+++ Public Access Journals Script .py
@@ -22,14 +22,10 @@ if Type == 'xlsx':
 else:
     print("Not valid")
  
-
-
-# In[ ]:
-
-
 for index, row in df.iterrows(): #Checks each row in the DOI columnn
     x = row['DOI']
     y = str(x)
+  
     #DOIs of Open Access Journals 
     if "10.1371/" in y or "10.3389/" in y or "10.3390/" in y or "10.1155/" in y or "10.3201/" in y:
         print(index, 'YES')
@@ -42,7 +38,7 @@ for index, row in df.iterrows(): #Checks each row in the DOI columnn
     elif "10.1139/" in y or "10.1101/" in y or "10.3923/" in y or "10.5539/" in y or "10.5344/" in y: 
         print(index, 'YES')
     #DOIs of articles that are available immediately or 6-12 months after the publication date
-    elif "10.1073/" in y or "10.1021/" in y:
+    elif "10.1073/" in y:
         print(index, 'YES (hybrid)')
     #DOIs of publishers that have both available and unavailable articles 
     elif "10.1177" in y:
@@ -56,22 +52,24 @@ for index, row in df.iterrows(): #Checks each row in the DOI columnn
         else:
             print(index, 'NO')
     elif "10.1128/" in y:
-        if "genomeA" in y or "msystems" in y or "msphere" in y or "mbio" in y or "CVI" in y:
+        if "genomeA" in y or "mSystems" in y  or "mSphere" in y or "mBio" in y or "CVI" in y:
             print(index, 'YES')
         else:
-            print(index, 'NO')     
+            print(index, 'NO') 
+    #EDIT THIS SECTION
     elif "10.1093/" in y or "10.3495/" in y or "10.2527/" in y:
         if "gigascience" in y or "gbe" in y or "aobpla" in y or "fqsafe" in y or "database" in y or "cdn" in y:
             print(index, 'YES')
         else:
             print(index, 'NO')
+    #EDIT THIS SECTION
     elif "10.1002/" in y or "10.1111/" in y or "10.3732/" in y :
-        if "fsn3" in y or "gcbb" in y or "pbi" in y or "tpj" in y or "ajb" in y  or "rse" in y:
+        if "fsn3" in y or "gcbb" in y or "pbi" in y or "tpj" in y or "ajb" in y  or "rse" in y or "mbo" in y:
             print(index, 'YES')
         else:
             print(index, 'NO')
     elif "10.2134/" in y or "10.3198/" in y:
-        if "ael" in y or "argonj" in y:
+        if "ael" in y or "agronj" in y:
             print(index, 'YES')
         else:
             print(index, 'NO')
@@ -83,25 +81,31 @@ for index, row in df.iterrows(): #Checks each row in the DOI columnn
     #DOIs of publications that are unavailable
     elif "10.1017/" in y or "10.4039/" in y or "10.1080/" in y or "10.21273/" in y or "10.2113/" in y:
         print(index, 'NO')
-    elif "10.17660/" in y or "10.1079/" in y or "10.3791/" in  or "10.1637/" in y: 
+    elif "10.17660/" in y or "10.1079/" in y or "10.3791/" in y or "10.1637/" in y: 
         print(index, 'NO')
     elif "10.3958/" in y or "10.1039/" in y or "10.5334/" in y or "10.2460/" in y:
         print(index, 'NO')
     #DOIs associated with Elsevier
-    elif "10.1016/" in y or "10.3168/" in y or "10.5232/" in y:    
-        url = 'https://api.elsevier.com/content/article/doi/' + str(x)
-        a = urllib.request.urlopen(url).read().decode('utf-8')
-        find = re.findall('<openaccessArticle>true', a)
-        find2 = re.findall('<openaccessArticle>false', a)
-        for i in find:
-            counter = Counter(find)
-            if counter == Counter({'<openaccessArticle>true': 1}):
-                print(index,'YES')
+    elif "10.1016/" in y or "10.3168/" in y or "10.5232/" in y:
+        try:
+            url = 'https://api.elsevier.com/content/article/doi/' + str(x)
+            a = urllib.request.urlopen(url).read().decode('utf-8')
+            find = re.findall('<openaccessArticle>true', a)
+            find2 = re.findall('<openaccessArticle>false', a)
+            for i in find:
+                counter = Counter(find)
+                if counter == Counter({'<openaccessArticle>true': 1}):
+                    print(index,'YES')
           
-        for j in find2:
-            counter = Counter(find2)
-            if counter == Counter({'<openaccessArticle>false': 1}):
-                print(index,'NO')
+            for j in find2:
+                counter = Counter(find2)
+                if counter == Counter({'<openaccessArticle>false': 1}):
+                    print(index,'NO')
+        except urllib.error.HTTPError as e:
+            if e.code >= 400:
+                print (index, 'NO')
+          
+        
     #DOIs associated with SpringerNature
     elif "10.1007/" in y or "10.4996/" in y: 
         try:
@@ -119,15 +123,14 @@ for index, row in df.iterrows(): #Checks each row in the DOI columnn
                 if counter == Counter({'"openaccess":"false"': 1}):
                     print(index,'NO')
         except urllib.error.HTTPError as e:
-            if e.code == 403:
+            if e.code >= 400:
                 print (index, 'NO')
+   
+                
     else:
         print(index, '')
 
-    
 
-
-# In[ ]:
 
 
 
